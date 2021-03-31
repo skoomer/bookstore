fd7ebd46ba6474abf18b7cdf56a211ad792b02c6
Ivan Tarasenko <ivan@Ivans-MacBook-Pro.local>
2021-04-01 00:37:01 +0300

    .gitignore fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it do
     expect do
-      described_class.create(email: 'george.zhukov@shiphawk.com',
+      described_class.create(email: 'goskoomer@gmail.com',
                              password: 'password')
     end.to change(described_class, :count).by(1)
   end
