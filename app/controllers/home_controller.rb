95be784a3a775553cb4ff5d6949cf97adefe54aa
Ivan Tarasenko <goskoomer@gmail.com>
2021-04-06 12:32:32 +0300

    add home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index; end
 
-  def home
-    render plain: 'Home page'
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   
-  root "home#home"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
   resources :photos, only: [:new, :create, :index, :edit, :update]
- 
- 
+  
+  
   post '/auth/:provider/callback', to: 'sessions#create'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/home", to: "home#index"
+  get "home/index"
+  root to: "home#home"
+
+
 end
