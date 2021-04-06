d560ccfde3509ab6120c36690b5073237e99c399
Ivan Tarasenko <goskoomer@gmail.com>
2021-04-06 12:55:52 +0300

    add home page heroku

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "home/index"
-  root to: "home#home"
+  root to: "home#index"
 
 
 end
