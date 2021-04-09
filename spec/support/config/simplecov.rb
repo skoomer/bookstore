25ac9d694d0785c5f612055aa01ac776498decdb
Ivan Tarasenko <goskoomer@gmail.com>
2021-04-09 19:49:50 +0300

    hot fix2

---
+++ spec/support/config/simplecov.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter 'app/channels/application_cable/'
+  add_filter 'app/jobs/'
+  add_filter 'app/mailers/'
+  add_filter 'app/models/application_record.rb'
+end
