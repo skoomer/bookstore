07babf8078f6477827da957ab7bcdb0078122e00
Ivan Tarasenko <goskoomer@gmail.com>
2021-04-11 17:00:09 +0300

    fix circleci db 1

--- spec/support/config/capybara.rb
+++ spec/support/config/capybara.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-
-
 Capybara.register_driver :selenium_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
 Capybara.default_driver = :selenium_chrome
-
