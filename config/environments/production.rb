d5f8455c42b018e83ffbb3f3341de8f8ecf118e9
Ivan <goskoomer@gmail.com>
2021-04-26 01:53:55 +0300

    fix  factorybot5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.seeds_enabled = true
+  config.seeds_enabled = false
 end
