bfc39409dc2fd1fa67fb745ca91c0f59e5a104e7
Ivan <goskoomer@gmail.com>
2021-04-25 23:19:27 +0300

    fix date migrate3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,7 @@
 # frozen_string_literal: true
-require 'ffaker'
 
-BOOK_MATERIAL = ['leather', 'glossy paper', 'matte paper'].freeze
-CATEGORIES = ['Mobile Development', 'Photo', 'Web Desing', 'Web Development'].freeze
-
-15.times { Author.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name) }
-
-CATEGORIES.each do |category|
-  Category.create(title: category)
-end
-
-15.times do
-  book = Book.create(
-    title: FFaker::Book.title,
-    price: rand(10.0..150.0).floor(2),
-    description: FFaker::Book.description(10),
-    publication_year: rand(2000...Time.zone.now.year),
-    height: rand(0.1...0.9).floor(1),
-    width: rand(0.1...0.9).floor(1),
-    depth: rand(0.1...0.9).floor(1),
-    material: BOOK_MATERIAL.sample,
-    quantity: rand(50),
-    category_id: Category.all.sample.id,
-    author_id: Author.all.sample.id
-
-  )
+if Rails.application.config.seeds_enabled
+  20.times do
+    FactoryBot.create :book, :attach_author
+  end
 end
