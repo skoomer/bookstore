ac2392d38b2b79614d4ada1d6c2f6aef2e8cea2d
Ivan <goskoomer@gmail.com>
2021-04-25 23:10:10 +0300

    fix date migrate2

--- db/migrate/20210415092643_create_categories.rb
+++ db/migrate/20210415092643_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
       t.string :title, null: false, unique: true
-      t.integer, :books_count, default: 0
+      t.integer :books_count, default: 0
       t.timestamps
     end
   end
