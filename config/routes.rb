4471b0806bdb05d6553184707b1172401cf0449f
Ivan <goskoomer@gmail.com>
2021-04-27 01:25:05 +0300

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  # get  '/catalog', to: 'categories#index', as: 'category'
-  # resources :catalog, only:[:index, :show], controller: 'categories'
   resources :books, only:[:index, :show]
 
-  
 end
