9030598ba61e11f69ce9424b651cc923c9cfc9f5
Ivan <goskoomer@gmail.com>
2021-04-27 01:25:05 +0300

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
   }
 
   resources :books, only: %i[index show]
+
 end
