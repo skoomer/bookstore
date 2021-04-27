baf6945cce74dbc16d5cbf745c3f22485539c327
Ivan <goskoomer@gmail.com>
2021-04-27 11:26:01 +0300

    add pages password

---
+++ app/helpers/new_password_helper.rb
@@ -0,0 +1,2 @@
+module NewPasswordHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :books, only:[:index, :show]
   resources :login, only:[:index]
   resources :signup, only:[:index]
+  resources :reset_password, only:[:index]
+  resources :new_password, only:[:index]
+
 
 
 end
