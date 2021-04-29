1303d644924356a7b46a864869df88cc87cd419f
Ivan <goskoomer@gmail.com>
2021-04-29 18:25:49 +0300

    add smtp

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  # default from: 'from@example.com'
   layout 'mailer'
+
+  default :from => 'any_from_address@example.com'
+  
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
 end
