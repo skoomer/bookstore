68f5e001560334d1e41997dd17d3379731561b14
Ivan <goskoomer@gmail.com>
2021-05-11 12:57:23 +0300

    remove stylesheets mod books controller partials

--- app/presenter/search_presenter.rb
+++ app/presenter/search_presenter.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class SearchPresenter < Rectify::Presenter
-  def search_books(search_title = nil)
-    if Books::FilterQuery::BOOK_FILTERING_ORDER[search_title]
-      return Books::FilterQuery::BOOK_FILTERING_ORDER[search_title]
-    end
-
-    Books::FilterQuery::DEFAULT_SORT || Books::FilterQuery::BOOK_FILTERING_ORDER[search_title]
+  def search_books(search_title)
+    Books::FilterQuery::BOOK_FILTERING_ORDER[search_title] || Books::FilterQuery::DEFAULT_SORT
   end
 end
