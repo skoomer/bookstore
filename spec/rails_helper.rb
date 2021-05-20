52be3510ad725467822a4659a0b02ecf957b08ac
Ivan <goskoomer@gmail.com>
2021-05-20 23:54:06 +0300

    add spec refact omni controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,9 @@ end
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include OmniAuthTestHelper, type: :controller
+
+  config.include StubFacebook
+
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
