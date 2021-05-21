6940dae5ed970c3c0af6c23c9dbe202a3bc86a3d
Ivan <goskoomer@gmail.com>
2021-05-21 14:49:19 +0300

    add feature spec controller spec locales

--- spec/support/config/stub_facebook.rb
+++ spec/support/config/stub_facebook.rb
@@ -3,7 +3,6 @@
 module StubFacebook
   def stub_env_for_omniauth_fb
     request.env['devise.mapping'] = Devise.mappings[:user]
-
     request.env['omniauth.auth'] = OmniAuth::AuthHash.new(
       provider: 'facebook',
       uid: '1234',
@@ -12,11 +11,6 @@ module StubFacebook
         name: 'Test User',
         image: 'http://example.com/image.jpg'
       }
-      # credentials: {
-      #   token: 'abcdefg12345',
-      #   refresh_token: '12345abcdefg',
-      #   expires_at: DateTime.now.in_time_zone
-      # }
     )
   end
 end
