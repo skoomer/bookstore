39c52efcf2bf319b26a6db2b303b1b9ec0285d78
Ivan <goskoomer@gmail.com>
2021-05-24 16:48:37 +0300

    fix send email

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
-
   }
+  get '/users', to: redirect('/users/sign_up')
 
   resources :books, only: %i[index show]
 end
