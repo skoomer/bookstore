2b62ffbf1ecee734a5cbf8fd56a30f8c023d5c11
Ivan <goskoomer@gmail.com>
2021-05-24 16:55:53 +0300

    add devise config host production

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root 'pages#index'
-
+  
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
