029e2f4d43dae212c771d6e2f2a85d2b70efca09
Ivan <goskoomer@gmail.com>
2021-05-24 16:48:37 +0300

    fix send email

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'pages#index'
-  
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
