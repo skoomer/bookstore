935a169b0dddb8b7e2d4fccc8baa1d69db7187b1
Ivan <goskoomer@gmail.com>
2021-05-26 17:17:15 +0300

    fix spec views model omniauth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,20 +59,11 @@ Rails.application.configure do
   # Raises error for missing translations.
   config.i18n.raise_on_missing_translations = true
   
-  config.action_mailer.perform_deliveries = true
   # config.action_view.annotate_rendered_view_with_filenames = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.seeds_enabled = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :port => 587,
-  :address => 'smtp.gmail.com',
-  :user_name => Rails.application.credentials.dig(:smtp, :user_name),
-  :password => Rails.application.credentials.dig(:smtp, :password),
-  :domain => 'gmail.com',
-  :authentication => :plain,
-  :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
--- spec/support/config/stub_facebook.rb
+++ spec/support/config/stub_facebook.rb
@@ -3,13 +3,11 @@
 module StubFacebook
   def stub_env_for_omniauth_fb
     request.env['devise.mapping'] = Devise.mappings[:user]
+
     request.env['omniauth.auth'] = OmniAuth::AuthHash.new(
-      provider: 'facebook',
-      uid: '1234',
       info: {
-        email: 'example@xyze.it',
-        name: 'Test User',
-        image: 'http://example.com/image.jpg'
+        email: FFaker::Internet.email,
+        name: FFaker::Name.name
       }
     )
   end
