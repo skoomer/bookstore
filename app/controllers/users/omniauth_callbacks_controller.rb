cc7ba2f575ea6a9ab7c2c2577d051f9ca6f4ce34
Ivan <goskoomer@gmail.com>
2021-06-01 22:17:28 +0300

    chagne omnifacebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,7 @@
 
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+      
     def facebook
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
@@ -9,6 +10,7 @@ module Users
         sign_in_and_redirect @user, event: :authentication
         success_flash_message if is_navigational_format?
       else
+        session['devise.facebook_data'] = request.env['omniauth.auth']
         failure_flash_message
       end
     end
@@ -16,13 +18,12 @@ module Users
     private
 
     def failure_flash_message
-      session['devise.facebook_data'] = request.env['omniauth.auth']
-      set_flash_message(:alert, :failure, kind: t('devise.omniauth.facebook'))
+      set_flash_message(:alert, :failure, kind: t('omniauth.facebook'))
       redirect_to new_user_registration_url
     end
 
     def success_flash_message
-      set_flash_message(:notice, :success, kind: t('devise.omniauth.facebook'))
+      set_flash_message(:notice, :success, kind: t('omniauth.facebook'))
     end
   end
 end
