3b880c4d71e0e19bcf61f85bda77906548516ddb
Ivan <goskoomer@gmail.com>
2021-06-03 17:22:43 +0300

    first push to git ActiveAdmin

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,28 +1,11 @@
 ActiveAdmin.register Book do
   decorate_with BookDecorator
   includes :category,  :author
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
+
   permit_params :title, :price, :description, :author_id, :category_id, :height, :width, :depth, :material, :quantity, :publication_year
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :price, :description, :author_id, :category_id, :height, :width, :depth, :material, :quantity, :publication_year]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+
   index do
     selectable_column
-
-    # column :covers do |book|
-    #   book.covers.map do |cover|
-    #     image_tag url_for(cover.variant(resize: '50x50').processed)
-    #   end
-    # end
 
     column :title
 
