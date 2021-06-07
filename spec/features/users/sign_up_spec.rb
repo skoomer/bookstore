e34c08a45899bafb5698516ecb1cfac258eedcf9
Ivan <goskoomer@gmail.com>
2021-06-07 18:21:13 +0300

    fix spec features factories config locales controller omniauth

--- spec/features/users/sign_up_spec.rb
+++ spec/features/users/sign_up_spec.rb
@@ -2,30 +2,43 @@
 
 RSpec.describe SignUp do
   let(:sign_up) { described_class.new }
-  let(:user) { build(:user) }
-  let(:home_page) { Home.new }
 
   before { sign_up.load }
 
   it { expect(sign_up).to be_all_there }
 
-  describe 'validations' do
-    it 'signs up with invalid data' do
-      sign_up.sign_up_button.click
-      expect(home_page).not_to have_selector 'div.alert.alert-success',
-                                             text: I18n.t('devise.registrations.signed_up')
+  context 'when signup valid data' do
+    let(:user) { build(:user) }
+
+    before do
+      sign_up.visit_and_sign_up_as(email: user.email, password: user.password)
     end
 
-    it 'signs up with valid data' do
-      sign_up.visit_and_sign_up_as(user.email, user.password)
-      expect(home_page).to have_current_path(root_path, ignore_query: true)
-      expect(home_page).to have_div_success(text: I18n.t('devise.registrations.signed_up'))
+    it 'redirects to root path' do
+      expect(sign_up).to have_current_path(root_path, ignore_query: true)
+    end
+
+    it 'sets success flash message' do
+      expect(sign_up).to have_success_flash_message
     end
 
     it 'hide links sign_up and log_in after success' do
-      sign_up.visit_and_sign_up_as(user.email, user.password)
-      expect(home_page).not_to have_link('a.sign_up_link')
-      expect(home_page).not_to have_link('a.log_in_link')
+      expect(sign_up).not_to have_sign_up_link
+      expect(sign_up).not_to have_log_in_link
+    end
+  end
+
+  context 'when invalid password or email' do
+    before do
+      sign_up.visit_and_sign_up_as
+    end
+
+    it 'redirects to root path' do
+      expect(sign_up).to have_current_path(user_registration_path)
+    end
+
+    it 'sets failure flash message' do
+      expect(sign_up).to have_error_message
     end
   end
 end
