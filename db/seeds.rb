add82ea471961548c0e655d2bd645f372e7a5a97
Ivan <goskoomer@gmail.com>
2021-06-09 18:51:07 +0300

    rebase develop fix conflict configure project

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,4 +25,7 @@ end
     author: Author.all.sample
   )
 end
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+User.create(email: 'exampl@gmail.com', password: 'Password1245')
