60d784be99e41d87ef4e58e3eb38fb7db6c3fc90
Ivan <goskoomer@gmail.com>
2021-06-09 14:22:29 +0300

    fix big conflict rebase

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,7 @@ class User < ApplicationRecord
          :omniauthable,
          omniauth_providers: %i[facebook]
 
-
   validates :password,
-
-
-
-
             format: { with: PASSWORD_FORMAT_REGEX }
 
   has_one  :shipping_address, dependent: :destroy
@@ -24,17 +19,6 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :shipping_address
   accepts_nested_attributes_for :billing_address
-
-
-
-            format: { with: PASSWORD_FORMAT_REGEX },
-            on: :create
-
-  belongs_to :shipping_address, class_name: 'Address', optional: true, autosave: true
-  belongs_to :billing_address, class_name: 'Address', optional: true, autosave: true
-  
-
-
 
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
