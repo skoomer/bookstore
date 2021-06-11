f3600fd87c4238e45c5c26b02557de30a303bec5
Ivan <goskoomer@gmail.com>
2021-06-11 17:09:19 +0300

    delete migration fix locales fix view review

--- db/migrate/20210518124833_add_book_association_to_review.rb
+++
@@ -1,5 +0,0 @@
-class AddBookAssociationToReview < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :reviews, :book, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_124833) do
+ActiveRecord::Schema.define(version: 2021_05_18_121659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,10 +131,10 @@ ActiveRecord::Schema.define(version: 2021_05_18_124833) do
     t.string "text"
     t.integer "score"
     t.integer "status", default: 0
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["book_id"], name: "index_reviews_on_book_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
