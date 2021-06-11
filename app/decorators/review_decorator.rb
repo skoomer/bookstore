5e8586d27750d9e0d142d0b82cd4879c500846f9
Ivan <goskoomer@gmail.com>
2021-06-11 18:50:28 +0300

    delete empty file change decorator

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -5,7 +5,6 @@ class ReviewDecorator < Draper::Decorator
 
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
-  #
   #   def created_at
   #     helpers.content_tag :span, class: 'time' do
   #       object.created_at.strftime("%a %m/%d/%y")
