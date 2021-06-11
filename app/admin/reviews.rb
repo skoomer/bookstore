5ba8012ee0ef9290a06960771e30f45fae78c85c
Ivan <goskoomer@gmail.com>
2021-06-11 17:12:06 +0300

    fix step 7 aws before pull step 6

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -1,22 +1,8 @@
 ActiveAdmin.register Review do
   config.filters = false
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :text, :score, :status, :user_id, :book_id
   permit_params :status, :active_admin_requested_event
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :text, :score, :status, :user_id, :book_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  
 
   after_save do |review|
     event = params[:review][:active_admin_requested_event]
