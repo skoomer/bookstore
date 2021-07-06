e199b99c1347cab9804fdb808e1144d3e90349db
Ivan <goskoomer@gmail.com>
2021-07-06 14:37:16 +0300

    fix author admin

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -5,7 +5,12 @@ ActiveAdmin.register Category do
   index do
     selectable_column
     column :title
+    # column I18n.t('active_admin.delete') do |category|
+    #   link_to I18n.t('active_admin.delete'), admin_category_path(category),
+    #           method: :delete, data: { confirm: t('admin.categories.confirmations', quantity: category.books.count) }
+    # end
     actions
   end
   
 end
+
